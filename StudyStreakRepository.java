8bf1990c3eaad4f0c57fdf090aec8bbade14b541
Harshitha8686 <harshithabashetty6@gmail.com>
2026-07-08 20:26:36 +0530

    Delete StudyStreakRepository.java

--- StudyStreakRepository.java
+++
@@ -1,8 +0,0 @@
-package com.slfs.repository;
-
-import com.slfs.model.StudyStreak;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyStreakRepository
-        extends JpaRepository<StudyStreak, Long> {
-}
