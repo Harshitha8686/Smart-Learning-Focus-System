3442cf27f5ef6370353bd245d7dd55da4bf2302b
Harshitha8686 <harshithabashetty6@gmail.com>
2026-07-08 20:24:17 +0530

    Delete TaskRepository.java

--- TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.slfs.repository;
-
-import com.slfs.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
