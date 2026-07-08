33f2ed88b569d5d5e13f85944b5271ac3dc36ab4
Harshitha8686 <harshithabashetty6@gmail.com>
2026-07-08 20:28:09 +0530

    Delete StudySessionRepository.java

--- StudySessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.slfs.repository;
-
-import com.slfs.model.StudySession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudySessionRepository
-        extends JpaRepository<StudySession, Long> {
-}
